c21431c0d62ee248d382bc0ce0f82d542f0fff60
Ilya Paddubny <ilyapaddubny@gmail.com>
2025-04-20 17:19:20 +0300

    Set up the project

---
+++ ABZTestTask/App/ABZTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABZTestTaskApp.swift
+//  ABZTestTask
+//
+//  Created by Ilya Paddubny on 20.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ABZTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
